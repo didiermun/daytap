9124fb33aa264b82f2c0a33d0439d3dd1dddfa78
Didien MUNEZERO <didiermunezer38@gmail.com>
2021-12-14 01:06:05 +0200

    fix: more types

--- src/report/entities/report.entity.ts
+++ src/report/entities/report.entity.ts
@@ -1,4 +1,9 @@
-// eslint-disable-next-line @typescript-eslint/no-empty-interface
-export interface Report {
-    report: User;
-}
+/* eslint-disable prettier/prettier */
+export class Report{
+    id: string
+    date: string;
+    location: string;
+    amount: string;
+    reporterId: string;
+  }
+  
