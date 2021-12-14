0658570b4101a078874d42b6694940c191fb7a44
Didien MUNEZERO <didiermunezer38@gmail.com>
2021-12-14 02:55:37 +0200

    fix: nodetype not found

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,21 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
+
+  const config = new DocumentBuilder()
+.setTitle('E-commerce') //the title you want for your swagger docs
+.setDescription('E-commerce API description') //description
+.setVersion('1.0')  //version setting for the docs
+.build();
+
+  const document = SwaggerModule.createDocument(app, config, {
+    include: [], //the modules that you want to include in your swagger docs
+  });
   await app.listen(3000);
 }
 bootstrap();
